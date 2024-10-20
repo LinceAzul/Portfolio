e1c61870dade3d18fa3e5d9c43443103cd102986
Ivan Iroslavov <ivaniroslavov@uma.es>
2024-10-20 11:27:07 +0200

    AboutCard changed

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -16,9 +16,9 @@ function AboutCard() {
             I'm always eager to work across various technologies and domains, with a particular interest in {" "} 
             <span className="red">Offensive Cybersecurity </span>.
             <br />
-            I am constantly seeking opportunities to learn and attend important events such as II and III Cybersecurity
-            Congress of Andalusia, ARITH 2024 (where I was part of the staff), DES 2023 and 2024, YOLO Vision 2024,
-            and several tech conferences on AI and cybersecurity across various cities in Spain.
+            I am constantly seeking opportunities to learn and attend important events such as RootedCON Malaga, UAD360,
+            II and III Cybersecurity Congress of Andalusia, ARITH 2024 (where I was part of the staff), DES 2023 and 2024,
+            YOLO Vision 2024, and several tech conferences on AI and cybersecurity across various cities in Spain.
             <br />
             <br />
             Aside from my technological interests, I also enjoy:
